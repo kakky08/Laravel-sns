25df411122005a58606959d111d439f63600b5fc
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2021-11-02 23:47:02 +0900

    5-2記事更新処理と記事削除処理の作成

--- laravel/resources/views/auth/register.blade.php
+++ laravel/resources/views/auth/register.blade.php
@@ -11,6 +11,8 @@
                     <div class="card-body text-center">
                         <h2 class="h3 card-title text-center mt-2">ユーザー登録</h2>
 
+                        @include('error_card_list')
+
                         <div class="card-text">
                             <form method="POST" action="{{ route('register') }}">
                                 @csrf
@@ -23,9 +25,13 @@
                                     <label for="email">メールアドレス</label>
                                     <input class="form-control" type="text" id="email" name="email" required value="{{ old('email') }}">
                                 </div>
+                                 <div class="md-form">
+                                    <label for="password">パスワード</label>
+                                    <input class="form-control" type="password" id="password" name="password" required>
+                                    </div>
                                 <div class="md-form">
                                     <label for="password_confirmation">パスワード(確認)</label>
-                                    <input class="form-control" type="password" id="password" name="password" required>
+                                    <input class="form-control" type="password" id="password_confirmation" name="password_confirmation" required>
                                 </div>
                                 <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ユーザ登録</button>
                             </form>
