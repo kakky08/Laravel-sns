fc8f0db0d18a335bcccb9247973db527c1ac75a4
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2021-11-02 23:56:46 +0900

    5-3記事の更新処理と記事削除処理の作成’

--- laravel/resources/views/articles/index.blade.php
+++ laravel/resources/views/articles/index.blade.php
@@ -40,7 +40,7 @@
                     <!-- dropdown -->
 
                     <!-- modal -->
-                    <div id="modal-dalete-{{ $article->id }}" class="modal fade" tabindex="-1" role="dialog">
+                    <div id="modal-delete-{{ $article->id }}" class="modal fade" tabindex="-1" role="dialog">
                         <div class="modal-dialog" role="document">
                             <div class="modal-content">
                                 <div class="modal-header">
